1fb3b460cba3b7b7d547dcb1764b3434b40f0ae8
Seri <seriorfali@gmail.com>
2015-12-13 13:51:10 -0800

    Added functions to show orders (with items) by cafe/user.

--- assets/controllers/ordersController.js
+++ assets/controllers/ordersController.js
@@ -2,12 +2,61 @@ var Order = require("../models/Order.js")
   , OrderItem = require("../models/OrderItem.js")
   , dbQuerier = require("../helpers/dbQuerier.js")
   
-function showAllCafeOrders(req, res) {
-	
+function showOrdersByParent(parentType, parentId, req, res) {
+	var dbGetOrders = new Promise(function(resolve, reject) {
+    var field = parentType + "_id"
+    dbQuerier.showDocsOfTypeByField("order", field, parentId, resolve, reject)
+  })
+  
+  dbGetOrders.then(function(orders) {
+    var getOrdersWithItems = new Promise(function(resolveLoop, rejectLoop) {
+      var ordersWithItems = []
+      
+      for (var i = 0; i < orders.length) {
+        var order = orders[i]
+        
+        var dbGetOrderItems = new Promise(function(resolve, reject) {
+          showDocsOfTypeByField("order_item", "order_id", order._id, resolve, reject)
+        })
+        
+        dbGetOrderItems.then(function(orderItems) {
+          order.items = orderItems
+          ordersWithItems.push(order)
+          
+          if (i === orders.length - 1) {
+            resolveLoop(ordersWithItems)
+          } else {
+            i++
+          }
+        })
+        .catch(function(err) {
+          rejectLoop(err)
+        })
+      }
+    })
+    
+    getOrdersWithItems.then(function(ordersWithItems) {
+      res.json(ordersWithItems)
+    })
+    .catch(function(err) {
+      console.log(err.message)
+      res.json({message: "Failed to retrieve order items."})
+    })
+  }
+  .catch(function(err) {
+    console.log(err.message)
+    res.json({message: "Failed to retrieve orders."})
+  })
 }
 
-function showAllUserOrders(req, res) {
-	
+function showCafeOrders(req, res) {
+  var cafeId = req.params.cafe_id
+	showOrdersByParent("cafe", cafeId, req, res)
+}
+
+function showUserOrders(req, res) {
+  var userId = req.params.user_id
+	showOrdersByParent("user", userId, req, res)
 }
 
 function showOrder(req, res) {
@@ -44,8 +93,9 @@ function deleteOrder(req, res) {
 }
 
 module.exports = {
-  showAllCafeOrders: showAllCafeOrders,
-  showAllUserOrders: showAllUserOrders,
+  showOrdersByParent: showOrdersByParent,
+  showCafeOrders: showCafeOrders,
+  showUserOrders: showUserOrders,
   showOrder: showOrder,
   editOrder: editOrder,
   deleteOrder: deleteOrder
